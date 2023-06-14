711d9334a287926e5546dd3c1091d7977e15b117
Abdelilah-Falih <121343981+Abdelilah-Falih@users.noreply.github.com>
2023-06-14 15:25:29 +0100

    main :  -> add Volley implementation (gradle)  -> add views( textview(quote & author)  -> send simple request to 'https://dummyjson.com/quotes/random' and get response;  -> convert the response to jsonObject and to String  -> finally show the response values in textViews (quote and author)

--- app/src/main/java/com/example/appminiprojet02/MainActivity.java
+++ app/src/main/java/com/example/appminiprojet02/MainActivity.java
@@ -3,9 +3,19 @@ package com.example.appminiprojet02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.appminiprojet02.databinding.ActivityMainBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +29,32 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         root = binding.getRoot();
         setContentView(root);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://dummyjson.com/quotes/random";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            binding.tvQuoteMain.setText(jsonObject.getString("quote"));
+                            binding.tvAuthorMain.setText(jsonObject.getString("author"));
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", error.getMessage() );
+            }
+        });
+
+        queue.add(stringRequest);
 
 
     }
